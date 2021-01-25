7b7ccc7b427d2ecab89285088b6fff558b020b1e
Vitali_Volski <Vitali_Volski@epam.com>
2021-01-25 10:04:04 +0300

    correction Optional task 2

--- src/Optional_task_2/Matrix.java
+++ src/Optional_task_2/Matrix.java
@@ -1,41 +1,49 @@
 package Optional_task_2;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Random;
 import java.util.Scanner;
+import java.util.stream.IntStream;
 
 
 public  class  Matrix {
 
-    public static void main(String[] args) {
+    public static void main(String[] arg) {
 
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
 
-        System.out.println("Enter number of lines and press <Enter>:");
-        int numberLines = scanner.nextInt();
+        System.out.println("Enter integer > 0 to set matrix lines "
+                + "and columns number, and press <Enter>:");
+        int numberMatrixLinesAndColumns = scanner.nextInt();
 
-        System.out.println("Enter number of columns and press <Enter>:");
-        int numberColumns = scanner.nextInt();
+        int[][] matrix = new int[numberMatrixLinesAndColumns]
+                [numberMatrixLinesAndColumns];
 
-        int[][] matrix = new int[numberLines][numberColumns];
-
-        for (int i = 0; i < numberLines; i++) {
-            for (int j = 0; j < numberColumns; j++) {
-                matrix[i][j] = (int) (Math.random() * (20 + 1) - 10);
-                System.out.print(matrix[i][j] + "\t");
-            }
-            System.out.print("\n");
-        }
+        System.out.println("Enter any integer number to set range for filling matrix "
+                + "fields and press <Enter>:");
+        int numberToFillMatrix = scanner.nextInt();
 
         System.out.print("\n");
-        System.out.print("Sort matrix by current column (3):\n");
-        for (int i = 0; i < numberColumns; i++) {
-            for (int j = 0; j < numberLines; j++) {
-                Arrays.sort(matrix, (o1, o2) -> o1[2] - o2[2]);
+        System.out.println("Matrix:");
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                matrix[i][j] = (int) (Math.random() * numberToFillMatrix);
+                System.out.print(matrix[i][j] + "\t");
+            }
+            System.out.println();
+        }
+        System.out.print("\n");
+        System.out.println("Sorting matrix descending by it's lines numbers sum:");
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                Arrays.sort(matrix, Comparator.comparingInt(linesNumbersSum
+                        -> 1 - IntStream.of(linesNumbersSum).sum()));
                 System.out.print(matrix[i][j] + "\t");
             }
             System.out.print("\n");
         }
     }
 }
+
